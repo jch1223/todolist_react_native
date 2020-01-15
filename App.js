2cf54f2d6cbec5d55eaec11244004e841ca713e8
jch <ico1828@gmail.com>
2020-01-15 19:26:17 +0900

    style basic

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View>
+        <Text>Open up App.js </Text>
+        <Text>to start working on your app!</Text>
+      </View>
     </View>
   );
 }
@@ -12,8 +15,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
   },
+  box: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    padding: 16,
+    margin: 64,
+    paddingBottom: 0,
+    borderWidth: 1,
+    borderColor: "#e0e0e0"
+  }
 });
