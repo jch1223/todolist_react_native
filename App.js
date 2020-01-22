19f1f65becb70f8c8e8a2a89569eb0842e2174d9
jch1223 <ico1828@gmail.com>
2020-01-23 08:34:28 +0900

    할일목록 state

--- App.js
+++ App.js
@@ -1,18 +1,40 @@
 import React from "react";
-import { StyleSheet, View, SafeAreaView } from "react-native";
+import { StyleSheet, FlatList, SafeAreaView } from "react-native";
 import Header from "./components/Header";
 import TodoItem from "./components/TodoItem";
 import TaskModal from "./components/TaskModal";
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Header />
-      <TodoItem title='리액트 네이티브 수강' done={true} />
-      <TodoItem title='리액트 네이티브 배포' done={false} />
-      <TaskModal isVisivle={false}></TaskModal>
-    </SafeAreaView>
-  );
+export default class App extends React.Component {
+  state = {
+    todos: [
+      {
+        title: "클래스 101 커리큘럼 만들기",
+        done: true
+      },
+      {
+        title: "할일 어플 만들기",
+        done: false
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Header />
+        <FlatList
+          data={this.state.todos}
+          renderItem={({ item }) => {
+            return <TodoItem title={item.title} done={item.done} />;
+          }}
+          keyExtractor={(_, index) => {
+            return `${index}`;
+          }}
+        />
+        <TaskModal isVisivle={false}></TaskModal>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
